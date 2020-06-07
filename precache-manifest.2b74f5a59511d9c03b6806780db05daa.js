3806f1e23d002fbdaea59f5477034dd85c0ce029
Horace Hung <horacegoal@gmail.com>
2020-06-07 19:30:16 +0800

    Updates

---
+++ precache-manifest.2b74f5a59511d9c03b6806780db05daa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b36e952e9a015aca1867d9a2c29be4db",
+    "url": "/PortfolioPage/index.html"
+  },
+  {
+    "revision": "7728251934a6e4842bd3",
+    "url": "/PortfolioPage/static/css/main.a07cc93d.chunk.css"
+  },
+  {
+    "revision": "37731ea247d1e96f998a",
+    "url": "/PortfolioPage/static/js/2.79ad7898.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PortfolioPage/static/js/2.79ad7898.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7728251934a6e4842bd3",
+    "url": "/PortfolioPage/static/js/main.5a49cfe6.chunk.js"
+  },
+  {
+    "revision": "38509dfb7e981859dfa0",
+    "url": "/PortfolioPage/static/js/runtime-main.12c7c9d3.js"
+  },
+  {
+    "revision": "342b0555417e579d8d6a4699202663f9",
+    "url": "/PortfolioPage/static/media/appdemo1.342b0555.gif"
+  },
+  {
+    "revision": "e2ac68227f994d7a4905e045d833442c",
+    "url": "/PortfolioPage/static/media/appdemo2.e2ac6822.gif"
+  }
+]);
